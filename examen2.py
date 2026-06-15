ed7eaa8bfb11552412b229d752f3f9bf6d4a71a5
JoseGersonGarciaRejas-dev <sebasalpi108@gmail.com>
2026-06-15 09:19:32 -0400

    Ejercicio 2

--- examen2.py
+++ examen2.py
@@ -1,6 +1,6 @@
-comentario = input("Ingresa tu comentario: ")
+comentario = input("Ingresa tu comentario: ").lower()
 
-if "malo" in comentario.lower():
+if "malo" in comentario:
     print("Alerta: Se ha detectado la palabra 'malo' en tu comentario.")
 else:
     print("Comentario aceptado.")
